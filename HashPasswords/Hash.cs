aeef7eccbef27457f40fd711121faf74fd00bd85
ReyLti <99005917+ReyLti@users.noreply.github.com>
2022-11-28 11:36:54 +0700

    Add files via upload

---
+++ HashPasswords/Hash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HashPasswords
+{
+    public class Hash
+    {
+        static public string Password(string Password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] sourceBytePassw = Encoding.UTF8.GetBytes(Password);
+                byte[] hashSourceBytePassw = sha256Hash.ComputeHash(sourceBytePassw);
+                string hashPassw = BitConverter.ToString(hashSourceBytePassw).Replace("-", string.Empty);
+                return hashPassw;
+            }
+        }
+    }
+}
